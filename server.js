c961532a24dbe9eebdd391f579af90c42eda8620
Matthew Costa <ucosty@gmail.com>
2016-05-12 20:09:02 +0100

    Added license and repo fields in package.json, updated server js to use strict mode, added docker file

--- server.js
+++ server.js
@@ -14,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with Indigo Nameserver.  If not, see <http://www.gnu.org/licenses/>.
 */
+"use strict";
 
 // Application Configuration
 var fs = require('fs');
